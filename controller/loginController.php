abf4a35d19b1e4f24fc0ed31d0c5156e90ded66d
YirsonAA <yirsonacevedo@gmail.com>
2023-03-13 23:23:26 -0600

    UsuariosApi

--- controller/loginController.php
+++ controller/loginController.php
@@ -1,6 +1,7 @@
 <?php
+//Aca es donde llamamos al modelo y este hace que conecte con la base de datos
 require_once "../model/usersModel.php";
-
+//usuarioExiste es una funcion del modelo: obtien la descripcion de ese usuario.
 $autDescription=usuarioExiste($_POST['name'],$_POST['pass']);
 
 if ($autDescription=="admin"){
